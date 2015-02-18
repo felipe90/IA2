e1b155dcb894989b72cee79f03312afa8e19d09c
AFelipe90 <lordganondorf@gmail.com>
2015-02-18 01:13:45 -0500

    Update addAncestros.pl

--- addAncestros.pl
+++ addAncestros.pl
@@ -1,2 +1,9 @@
-asserta(hombre(juan)).
+asserta(madre(martha,margarita)).
 
+asserta(padre(mario,margarita)).
+
+asserta(madre(ana,mario)).
+asserta(madre(ana,juan)).
+
+asserta(padre(pedro,mario)).
+asserta(padre(pedro,mjuan)).
